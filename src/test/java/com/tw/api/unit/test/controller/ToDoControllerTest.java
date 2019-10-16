c68ba4f25eecbe39ff6138c978d52d802b9e52a5
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-16 18:23:27 +0800

    Add for GetMapping tests

---
+++ src/test/java/com/tw/api/unit/test/controller/ToDoControllerTest.java
@@ -0,0 +1,89 @@
+package com.tw.api.unit.test.controller;
+
+
+import com.tw.api.unit.test.domain.todo.Todo;
+import com.tw.api.unit.test.domain.todo.TodoRepository;
+import com.tw.api.unit.test.services.ShowService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(TodoController.class)
+@ActiveProfiles(profiles = "test")
+
+public class ToDoControllerTest {
+
+//    @Autowired
+//    private TodoController todoController;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private TodoRepository todoRepository;
+
+    @Test
+    void getAll() throws Exception {
+        //given
+        List<Todo> todoList = new ArrayList<>();
+        Todo todo = new Todo(1, "mytestTodo", false, 2);
+        todoList.add(todo);
+
+        when(todoRepository.getAll()).thenReturn(todoList);
+        //when
+        ResultActions result = mvc.perform(get("/todos"));
+        //then
+        result.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[0].title", is("mytestTodo")))
+                .andExpect(jsonPath("$[0].completed", is(false)))
+                .andExpect(jsonPath("$[0].order", is(2)));
+
+    }
+
+    @Test
+    void getTodo() throws Exception {
+        //given
+        List<Todo> todoList = new ArrayList<>();
+        Todo todo1 = new Todo(1, "doTestCode", false, 1);
+        Todo todo2 = new Todo(2, "eatmyLunch", false, 2);
+        todoList.add(todo1);
+        todoList.add(todo2);
+
+        when(todoRepository.findById(todo1.getId())).thenReturn(Optional.of(todo1));
+        //when
+        ResultActions result = mvc.perform(get("/todos/1"));
+        //then
+        result.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.title", is("doTestCode")))
+                .andExpect(jsonPath("$.completed", is(false)))
+                .andExpect(jsonPath("$.order", is(1)));
+
+    }
+
+
+}
